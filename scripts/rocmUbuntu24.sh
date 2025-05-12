b8d2425bf6b073ee5a2eba7130847ed8fe86998c
Nico Luhmann <luhmann.nico@gmail.com>
2025-05-12 22:12:29 +0200

    updated rocm script for machines that support rocm

--- scripts/rocmUbuntu24.sh
+++ scripts/rocmUbuntu24.sh
@@ -8,7 +8,7 @@ source "$SCRIPT_DIR/utils/utils.sh"
 install_packages wget
 
 wget https://repo.radeon.com/amdgpu-install/6.4/ubuntu/noble/amdgpu-install_6.4.60400-1_all.deb
-sudo apt install ./amdgpu-install_6.4.60400-1_all.deb
-sudo apt update
-sudo apt install "linux-headers-$(uname -r)" "linux-modules-extra-$(uname -r)"
-sudo apt install amdgpu-dkms
+sudo apt install ./amdgpu-install_6.4.60400-1_all.deb -y
+sudo apt update -y
+sudo apt install "linux-headers-$(uname -r)" "linux-modules-extra-$(uname -r)" -y
+sudo apt install amdgpu-dkms -y
