b6fb3244128322d3dc9318d91576604b7dc122be
Nico Luhmann <luhmann.nico@gmail.com>
2025-05-24 13:14:25 +0200

    Removed wezterm, Implement unified Ubuntu setup script for version detection and package installation

--- scripts/ubuntuSetup.sh
+++ scripts/ubuntuSetup.sh
@@ -5,15 +5,30 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # Source utils.sh from parent directory
 source "$SCRIPT_DIR/utils.sh"
 
-install_packages libfuse2 curl git tar stow timeshift tmux
+# Get Ubuntu version
+UBUNTU_VERSION=$(lsb_release -rs)
+
+# install common packages
+install_packages curl git tar stow timeshift tmux
+if [[ "$UBUNTU_VERSION" == "22.04" ]]; then
+  echo "Detected Ubuntu 22.04"
+  install_packages libfuse2
+elif [[ "$UBUNTU_VERSION" == "24.04" ]]; then
+  echo "Detected Ubuntu 24.04"
+  install_packages libfuse2t64
+else
+  echo "Unsupported Ubuntu version: $UBUNTU_VERSION"
+  exit 1
+fi
+
 
 source "$SCRIPT_DIR/source/docker.sh"
 source "$SCRIPT_DIR/source/neovim.sh"
 source "$SCRIPT_DIR/source/vscode.sh"
 source "$SCRIPT_DIR/source/obsidian.sh"
-source "$SCRIPT_DIR/source/starship.sh"
 source "$SCRIPT_DIR/source/nerdFonts.sh"
-source "$SCRIPT_DIR/source/wezterm.sh"
+# source "$SCRIPT_DIR/source/terminal.sh"
+source "$SCRIPT_DIR/source/starship.sh"
 source "$SCRIPT_DIR/source/todoist.sh"
 source "$SCRIPT_DIR/source/gnomeSetup.sh"
 source "$SCRIPT_DIR/source/brave.sh"
