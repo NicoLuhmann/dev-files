1cc6cf0d9c79fcf5022318976b3051b5e35def63
NicoLuhmann <luhmann.nico@gmail.com>
2025-06-19 23:48:14 +0200

    Update Neovim configuration and add ripgrep settings

--- .config/nvim/lua/config/mini.lua
+++ .config/nvim/lua/config/mini.lua
@@ -4,16 +4,27 @@ require("mini.surround").setup({
 })
 require("mini.comment").setup({})
 require("mini.ai").setup({})
+require("mini.move").setup({})
+require("mini.jump").setup({
+  delay = {
+    highlight = 10000000,
+  }
+})
+require("mini.splitjoin").setup({})
 
 if not vim.g.vscode then
-    require("mini.indentscope").setup({})
-    require("mini.icons").setup({})
-    require("mini.pairs").setup({})
-    require("mini.git").setup({})
-    require("mini.diff").setup({})
-    require("mini.statusline").setup({})
-    require("mini.tabline").setup({})
-    require("mini.files").setup({
-        keymap.set("n", "<leader>e", ":lua MiniFiles.open()<CR>", { silent = true }),
-    })
+  require("mini.indentscope").setup({})
+  require("mini.icons").setup({})
+  require("mini.pairs").setup({})
+  require("mini.git").setup({})
+  require("mini.diff").setup({})
+  require("mini.statusline").setup({})
+  require("mini.tabline").setup({})
+  require("mini.notify").setup({})
+  require("mini.cursorword").setup({})
+  require("mini.completion").setup({})
+  require("mini.pick").setup({})
+  require("mini.files").setup({
+    keymap.set("n", "<leader>e", ":lua MiniFiles.open()<CR>", { silent = true }),
+  })
 end
--- .config/nvim/lua/config/setup.lua
+++ .config/nvim/lua/config/setup.lua
@@ -1,11 +1,4 @@
 local keymap = vim.keymap
--- move lines vertically
-keymap.set("n","<M-j>",":m +1<CR>")
-keymap.set("n","<M-k>",":m -2<CR>")
-keymap.set("i","<M-j>","<Esc>:m +1<CR>gi")
-keymap.set("i","<M-k>","<Esc>:m -2<CR>gi")
-keymap.set("v","<M-j>",":m '>+1<CR>gv=gv")
-keymap.set("v","<M-k>",":m '<-2<CR>gv=gv")
 
 -- Check if running in VSCode's Neovim extension
 if vim.g.vscode then
