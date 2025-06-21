0b7cb281d30b653add4bea1e781b0b05b6cd8172
NicoLuhmann <luhmann.nico@gmail.com>
2025-06-21 13:24:34 +0200

    nvim: add flash.nvim, make mini.surround behave like tpope surround

--- .config/nvim/lua/plugins/mini-nvim.lua
+++ .config/nvim/lua/plugins/mini-nvim.lua
@@ -4,7 +4,22 @@ return {
     config = function()
         local keymap = vim.keymap
         require("mini.surround").setup({
+            custom_surroundings = {
+                ['(']= { output = { left = '( ', right = ' )' } },
+                ['['] = { output = { left = '[ ', right = ' ]' } },
+                ['{'] = { output = { left = '{ ', right = ' }' } },
+                ['<'] = { output = { left = '< ', right = ' >' } },
+            },
             search_method = "cover_or_next",
+            mappings = {
+                add = "ys",
+                delete = "ds",
+                find = "",
+                find_left = "",
+                highlight = "",
+                replace = "cs",
+                update_n_lines = "",
+            },
         })
         require("mini.comment").setup({})
         require("mini.ai").setup({})
