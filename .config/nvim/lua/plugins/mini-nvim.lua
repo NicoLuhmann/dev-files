140dcb7dfebbd304e5bb1b5fa5c0436328fd5daa
NicoLuhmann <luhmann.nico@gmail.com>
2025-06-22 22:52:34 +0200

    nvim: added blink autocompletion, removed mini completion, added noice

--- .config/nvim/lua/plugins/mini-nvim.lua
+++ .config/nvim/lua/plugins/mini-nvim.lua
@@ -1,46 +1,45 @@
 return {
-    "echasnovski/mini.nvim",
-    version = false,
-    config = function()
-        local keymap = vim.keymap
-        require("mini.surround").setup({
-            custom_surroundings = {
-                ['(']= { output = { left = '( ', right = ' )' } },
-                ['['] = { output = { left = '[ ', right = ' ]' } },
-                ['{'] = { output = { left = '{ ', right = ' }' } },
-                ['<'] = { output = { left = '< ', right = ' >' } },
-            },
-            search_method = "cover_or_next",
-            mappings = {
-                add = "ys",
-                delete = "ds",
-                find = "",
-                find_left = "",
-                highlight = "",
-                replace = "cs",
-                update_n_lines = "",
-            },
-        })
-        require("mini.comment").setup({})
-        require("mini.ai").setup({})
-        require("mini.move").setup({})
-        require("mini.jump").setup({
-            delay = {
-              highlight = 10000000,
-            }
-        })
-        require("mini.splitjoin").setup({})
+	"echasnovski/mini.nvim",
+	version = false,
+	config = function()
+		local keymap = vim.keymap
+		require("mini.surround").setup({
+			custom_surroundings = {
+				["("] = { output = { left = "( ", right = " )" } },
+				["["] = { output = { left = "[ ", right = " ]" } },
+				["{"] = { output = { left = "{ ", right = " }" } },
+				["<"] = { output = { left = "< ", right = " >" } },
+			},
+			search_method = "cover_or_next",
+			mappings = {
+				add = "ys",
+				delete = "ds",
+				find = "",
+				find_left = "",
+				highlight = "",
+				replace = "cs",
+				update_n_lines = "",
+			},
+		})
+		require("mini.comment").setup({})
+		require("mini.ai").setup({})
+		require("mini.move").setup({})
+		require("mini.jump").setup({
+			delay = {
+				highlight = 10000000,
+			},
+		})
+		require("mini.splitjoin").setup({})
 
-        if not vim.g.vscode then
-            require("mini.icons").setup({})
-            require("mini.pairs").setup({})
-            require("mini.git").setup({})
-            require("mini.diff").setup({})
-            require("mini.statusline").setup({})
-            require("mini.tabline").setup({})
-            require("mini.notify").setup({})
-            require("mini.cursorword").setup({})
-            require("mini.completion").setup({})
-        end
-    end,
+		if not vim.g.vscode then
+			require("mini.icons").setup({})
+			require("mini.pairs").setup({})
+			require("mini.git").setup({})
+			require("mini.diff").setup({})
+			require("mini.statusline").setup({})
+			require("mini.tabline").setup({})
+			require("mini.notify").setup({})
+			require("mini.cursorword").setup({})
+		end
+	end,
 }
