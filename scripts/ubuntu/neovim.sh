758566a576813f436168b4c590d97a74ba498314
NicoLuhmann <luhmann.nico@gmail.com>
2025-06-27 20:18:47 +0200

    updated neovim.sh script

--- scripts/ubuntu/neovim.sh
+++ scripts/ubuntu/neovim.sh
@@ -1,7 +1,40 @@
 #!/bin/bash
+set -e
 
-install_packages jq xclip ripgrep build-essential luarocks 
+# Install dependencies
+sudo apt update
+sudo apt install -y curl git wget xclip ripgrep build-essential luarocks gzip tar unzip
 
-curl -s https://api.github.com/repos/neovim/neovim/releases/latest | jq -r '.assets[] | select(.name == "nvim-linux-x86_64.appimage") | .browser_download_url' | xargs curl -LO
-chmod u+x nvim-linux-x86_64.appimage
-sudo mv nvim-linux-x86_64.appimage /usr/local/bin/nvim
+# Install tiktoken_core for Lua 5.1
+sudo luarocks install --lua-version 5.1 tiktoken_core
+
+# Install latest lazygit
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": *"v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+sudo install lazygit -D -t /usr/local/bin/
+rm lazygit.tar.gz lazygit
+
+# Install Neovim
+NVIM_VERSION="0.11.2"
+arch=$(uname -m)
+
+declare -A nvim_archives=(
+  ["x86_64"]="nvim-linux-x86_64"
+  ["aarch64"]="nvim-linux-arm64"
+)
+
+if [[ -n "${nvim_archives[$arch]}" ]]; then
+  NVIM_ARCHIVE="${nvim_archives[$arch]}"
+  curl -LO "https://github.com/neovim/neovim/releases/download/v${NVIM_VERSION}/${NVIM_ARCHIVE}.tar.gz"
+  sudo rm -rf "/opt/${NVIM_ARCHIVE}"
+  sudo tar -C /opt -xzf "${NVIM_ARCHIVE}.tar.gz"
+  export_line="export PATH=\"\$PATH:/opt/${NVIM_ARCHIVE}/bin\""
+  if ! grep -Fxq "$export_line" "$HOME/.bashrc"; then
+    echo "$export_line" >> "$HOME/.bashrc"
+  fi
+  rm "${NVIM_ARCHIVE}.tar.gz"
+else
+  echo "Unsupported architecture: $arch"
+  exit 1
+fi
