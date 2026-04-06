03fe822efb3b06a42bc16a82ed6db0e3cb17512e
NicoLuhmann <luhmann.nico@gmail.com>
2026-04-06 20:18:30 +0200

    feat(setup): rm devpod, add lazygit and nvm scripts

---
+++ scripts/ubuntu/lazygit.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+arch="$(uname -m)"
+
+declare -A lazygit_archives=(
+  ["x86_64"]="Linux_x86_64"
+  ["amd64"]="Linux_x86_64"
+  ["aarch64"]="Linux_arm64"
+  ["arm64"]="Linux_arm64"
+)
+
+if [[ -n "${lazygit_archives[$arch]}" ]]; then
+  LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": *"v\K[^"]*')
+  LAZYGIT_ARCHIVE="${lazygit_archives[$arch]}"
+  curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_${LAZYGIT_ARCHIVE}.tar.gz"
+  tar xf lazygit.tar.gz lazygit
+  sudo install lazygit -D -t /usr/local/bin/
+  rm lazygit.tar.gz lazygit
+  echo "Lazygit installed successfully."
+else
+  echo "Unsupported architecture: $arch" >&2
+  exit 1
+fi
